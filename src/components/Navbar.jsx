99bdf288767a281f570bc981eb4d36bdc17daccd
impriyanka60 <btech15060.22@bitmesra.ac.in>
2025-04-12 15:31:25 +0530

    2nd

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+     <nav style={{display:"flex"}}>
+        <ul>home</ul>
+        <ul>services</ul>
+     </nav>
+    </div>
+  )
+}
+
+export default Navbar
